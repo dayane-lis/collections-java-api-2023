6e50d08c4969a01f0a85b82fb3d0f91e11875c85
Dayane Silva <day.nemesis@gmail.com>
2023-09-10 23:58:04 -0300

    Inclusão das classes CarrinhoCompras e CatalogoLivros

--- src/main/java/list/OperacoesBasicas/CarrinhoCompras.java
+++ src/main/java/list/OperacoesBasicas/CarrinhoCompras.java
@@ -1,4 +1,85 @@
 package main.java.list.OperacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List
+
 public class CarrinhoCompras {
+    //atributos
+    private List<Item> itemList;
+
+    public CarrinhoCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome. preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itensParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itensParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQuant();
+                valorTotal += valorItem;  //valorTotal = valorTotal + valorItem
+            }
+            return valorTotal;
+        } else  {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else  {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoCompras{" +
+                "itemList=" + itemList +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        //Criando uma instância do carrinho de compras
+        CarrinhoCompras carrinhoCompras = new CarrinhoCompras();
+
+        //Adicionando itens ao carrinho
+        carrinhoCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoCompras.adicionarItem("Caderno", 35d, 1);
+        carrinhoCompras.adicionarItem("Borracha", 2d, 2);
+
+        //Exibindo itens no carrinho
+        carrinhoCompras.exibirItens();
+
+        //Removendo itens do carrinho
+        carrinhoCompras.removerItem("Lápis");
+
+        //Exibindo itens atualizados no carrinho
+        carrinhoCompras.exibirItens();
+
+        //Calculadndo e exibindo o valoer total da compra
+        System.out.println("O valor total da compra é = " + carrinhoCompras.calcularValorTotal());
+    }
 }
+
+//Verificar onde estão os erros de código
